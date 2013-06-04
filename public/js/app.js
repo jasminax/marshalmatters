f10f892225e06491e92ab75c7b3efd900a80e088
Jasmina Xie <jasmina.xie@gmail.com>
2013-06-04 00:52:07 -0700

    Added minimum female check

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 	var form = '<form><fieldset><input type="text" name="paddlername" placeholder="Name"/>\
 		<input type="text" name="paddlerweight" placeholder="Weight"/>\
 		<label class="gender-selection radio inline">\
-		<input type="radio" name="genderselection" id="malepaddler" value="male">Male</input>\
+		<input type="radio" name="genderselection" class="malepaddler" value="male">Male</input>\
 		</label>\
 		<label class="gender-selection radio inline">\
-		<input type="radio" name="genderselection" id="femalepaddler" value="female">Female</input>\
+		<input type="radio" name="genderselection" class="femalepaddler" value="female">Female</input>\
 		</label>\
 		<br/>\
 		<label class="paddler-side checkbox inline">\
@@ -22,6 +22,21 @@ $(document).ready(function(){
 
 	$('.row').find('li').append(form);
 
+	var minimumGirls = $('#female-minimum').val();
+
+	var checkMinimumGirls = function() {
+		var females = $('.femalepaddler:checked').length;
+		console.log(females);
+		console.log(minimumGirls);
+		if (females < minimumGirls) {
+			$("#female-status").html("<span class='alert alert-error'>Too many dicks!</span>")
+		}
+		else {
+			$("#female-status").html("<span class='alert alert-success'>Enough chicks.</span>")
+		}
+	}
+
+	$('input[type=radio]').on("click", checkMinimumGirls);
 	
 
 });
